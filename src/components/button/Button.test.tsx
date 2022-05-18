41d7d1bbd3fa6678a6871cc3d26ecf3fcb1502c4
Bradley Martin <bradley.r.martin@me.com>
2022-05-18 13:04:30 +1000

    Init

---
+++ src/components/button/Button.test.tsx
@@ -0,0 +1,9 @@
+import Button from "./Button";
+import React from "react";
+import { render } from "@testing-library/react";
+
+describe("Button", () => {
+  test("renders the Button component", () => {
+    render(<Button label="Hello world!" />);
+  });
+});
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import './Button.css'
 
+import React from "react";
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className="button">{props.label}</button>;
 };
 
 export default Button;
