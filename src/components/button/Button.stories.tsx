283cc53d38cc6f42c366db4503c9f1d7c037f54d
Bradley Martin <bradley.r.martin@me.com>
2022-05-18 16:17:24 +1000

    upgrade

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -15,7 +15,7 @@ const Template: ComponentStory<typeof Button> = (args:any) => <Button {...args} 
 export const HelloWorld = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 HelloWorld.args = {
-  label: "Hello world!",
+  label: "A test of button!",
 };
 
 export const ClickMe = Template.bind({});
